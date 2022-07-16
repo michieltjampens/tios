546ded18f2f731b4b8cab19130f206d534c39d01
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2022-07-16 18:47:57 +0200

    updated

--- Core/code/uboot/board/iostuff/tios-6ull/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_board/iostuff/tios-6ull/built-in.o :=  arm-none-linux-gnueabihf-ld.bfd   -r -o board/iostuff/tios-6ull/built-in.o board/iostuff/tios-6ull/tios-6ull.o board/iostuff/tios-6ull/mmc.o 
